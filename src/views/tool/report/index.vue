1ee8bb1abcea682818b5146b2bf4354384cf102c
Young <1065569578@qq.com>
2023-07-19 13:55:07 +0800

    20230719

---
+++ src/views/tool/report/index.vue
@@ -0,0 +1,18 @@
+
+
+<template>
+  <div></div>
+</template>
+<script>
+export default {
+  name: "Report",
+  data() {
+    return {
+      src: process.env.VUE_APP_BASE_API + "/ares/tool/report/index",
+    };
+  },
+  mounted: function () {
+    window.open(this.src);
+  },
+};
+</script>
