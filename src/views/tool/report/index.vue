f4cb07e4e3365c3c85544e70d42ad12c0fd3b888
Young <1065569578@qq.com>
2023-07-20 09:15:54 +0800

    20230720

--- src/views/tool/report/index.vue
+++ src/views/tool/report/index.vue
@@ -1,7 +1,9 @@
 
 
 <template>
-  <div></div>
+  <div v-loading="loading" :style="'height:'+ height">
+    <iframe :src="src" frameborder="no" style="width: 100%;height: 100%" scrolling="auto" />
+  </div>
 </template>
 <script>
 export default {
@@ -9,10 +11,18 @@ export default {
   data() {
     return {
       src: process.env.VUE_APP_BASE_API + "/ares/tool/report/index",
+      height: document.documentElement.clientHeight - 94.5 + "px;",
+      loading: true,
     };
   },
   mounted: function () {
-    window.open(this.src);
+    setTimeout(() => {
+      this.loading = false;
+    }, 230);
+    const that = this;
+    window.onresize = function temp() {
+      that.height = document.documentElement.clientHeight - 94.5 + "px;";
+    };
   },
 };
 </script>
